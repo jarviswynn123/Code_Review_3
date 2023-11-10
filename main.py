ea9bab7474235c2938809db80ceab5c5bc99adfc
jarviswynn123 <jmwynn11998@gmail.com>
2023-11-09 23:02:38 -0500

    question 1 answered

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+contestants = [
+    ["Binkie", 1, False, []],
+    ["Butterball", 3, True, ["herding sheep", "singing", "fetch", "pulling sleds"]],
+    ["Kerby", 5, True, ["swimming"]],
+    ["Lucy", 5, False, ["fetch", "solving puzzles"]],
+    ["Toaster", 2, False, []],
+    ["Fifi", 1, False, []],
+    ["Noodles", 4, True, ["pulling sleds"]]
+]
+
+
+labels = ["name", "age", "does_tricks", "other_talents"]
